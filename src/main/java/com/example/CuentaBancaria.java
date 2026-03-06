67a9f0d1538dd8f95c01bc616be1cec4f1864b7a
Alejandro-Gomez-Garcia <444alejogarcia@gmail.com>
2026-03-06 10:19:22 -0500

    primer commit

---
+++ src/main/java/com/example/CuentaBancaria.java
@@ -0,0 +1,30 @@
+package com.example;
+
+public class CuentaBancaria {
+
+    private String titular;
+    private String saldo;
+
+    public CuentaBancaria(String titular, String saldo) {
+        this.titular = titular;
+        this.saldo = saldo;
+
+        if  (getSaldo < 0){
+            
+        }
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public String getSaldo() {
+        return saldo;
+    }
+
+
+}
